c58856a9b91c43cee7a3b80b4e2f7ef3caafbf2e
Manuel Familia <manuelgfx@gmail.com>
2015-04-02 16:38:05 -0400

    trying out blueprints to include aviary

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 module.exports = {
   name: 'ember-aviary',
   included: function(app) {
+    this._super.included(app);
     app.import('bower_components/aviary/index.js');
     app.import('vendor/icons/edit.svg', { destDir: 'assets/images' });
   }
