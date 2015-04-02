46d904b6d0614c050321b714a368240eb9dc2d96
Manuel Familia <manuelgfx@gmail.com>
2015-04-02 15:28:48 -0400

    Adding aviary as bower dependency Removing script injection of aviary lib

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,5 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  var ENV = {
-    contentSecurityPolicy: {
-      'script-src': "'self' https://dme0ih8comzn4.cloudfront.net/js/feather.js"
-    }
-  };
-  return ENV;
+  return {};
 };
--- index.js
+++ index.js
@@ -3,11 +3,6 @@
 
 module.exports = {
  name: 'ember-aviary',
- contentFor: function(type) {
-   if(type === 'body') {
-     return '<script type="text/javascript" src="https://dme0ih8comzn4.cloudfront.net/js/feather.js"></script>';
-   }
- },
  included: function(app) {
   app.import('vendor/icons/edit.svg', { destDir: 'assets/images' });
  }
