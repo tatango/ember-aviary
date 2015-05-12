588b357ce17088de821c3eb14c17905cf8654848
kagemusha <mmadrid99@gmail.com>
2015-05-12 09:01:32 -0400

    init on didInsertElement

--- addon/services/aviary-api-client.js
+++ addon/services/aviary-api-client.js
@@ -6,14 +6,14 @@ export default Ember.Object.extend({
   featherEditor: null,
 
   launch: function(options, initOptions) {
-    var featherEditor = this.get('featherEditor');
-    if (!featherEditor) {
-      this.initFeatherEditor(initOptions);
-    }
     this.get('featherEditor').launch(options);
   },
 
   initFeatherEditor: function(initOptions){
+    if (this.get('featherEditor')) {
+      return;
+    }
+
     var apiKey = (initOptions && initOptions.apiKey) || this.get('apiKey');
     var theme = (initOptions && initOptions.theme) || this.get('theme');
     var aviaryFeather = new Aviary.Feather({
