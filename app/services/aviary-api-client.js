0ce133c70d06af031710c6ea9278c0c44b971db9
Ken Sin <kensin1129@gmail.com>
2015-05-12 11:36:52 -0400

    Move environment into addon service. Only exports in app/

--- app/services/aviary-api-client.js
+++ app/services/aviary-api-client.js
@@ -1,16 +1,3 @@
-import Ember from 'ember';
-import ENV from '../config/environment';
 import AviaryApiClientService from 'ember-aviary/services/aviary-api-client';
 
-export default AviaryApiClientService.extend({
-  apiKey: function() {
-    var apiKey = ENV.AVIARY_API_KEY || Ember.$('meta[property="aviary:api_key"]').attr('content') || window.AVIARY_API_KEY;
-    if (!apiKey) { // seems to work without api key so don't throw error, just log if it's missing
-      Ember.Logger.info('Ember Aviary Service: Missing Aviary key, please set `ENV.AVIARY_API_KEY` in config.environment.js');
-    }
-    return apiKey;
-  }.property(),
-  theme: function() {
-    return ENV.AVIARY_THEME || Ember.$('meta[property="aviary:theme"]').attr('content') || window.AVIARY_THEME;
-  }.property()
-});
+export default AviaryApiClientService.extend();
